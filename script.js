cdddd77a5d4ff31b3866a7adeadabf0a33475ffe
catkitty12021 <80436885+catkitty12021@users.noreply.github.com>
2023-11-25 22:43:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// hello there!
+const grid = [[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1]];
+
+$(document).ready(function() {
+	for (let i=0; i<grid.length; i++;) {
+		for (let j=0; j<grid[i].length; j++;) {
+			// hello
+		}
+	}
+});
